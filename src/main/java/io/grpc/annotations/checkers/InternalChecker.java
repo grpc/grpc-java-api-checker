c17c887879f89e3fd4f8f0512161e35613205108
Eric Gribkoff <ericgribkoff@google.com>
2018-03-21 11:05:31 -0700

    make var final, change to allow any non-internal declaration

--- src/main/java/io/grpc/annotations/checkers/InternalChecker.java
+++ src/main/java/io/grpc/annotations/checkers/InternalChecker.java
@@ -37,8 +37,7 @@ import javax.lang.model.element.AnnotationMirror;
 public final class InternalChecker extends AnnotationChecker {
 
   public InternalChecker() {
-    super("io.grpc.Internal");
-    checkTopOfMethodHierarchy = true;
+    super("io.grpc.Internal", true);
   }
 
   @Override
